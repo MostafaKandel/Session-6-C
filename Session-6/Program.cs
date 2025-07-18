1174f81e40add3dea06638041663cff2216ea710
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-18 20:50:38 +0200

    Q2

--- Session-6/Program.cs
+++ Session-6/Program.cs
@@ -10,7 +10,11 @@
         public static void ChangeInputValueByRef(ref int x) {
             x = 10;
         }
-        
+
+        public static void SumAndSubValues(int num1, int num2, out int sum, out int sub) {
+                sum= num1 + num2;
+                sub= num1 - num2;
+        }
         static void Main(string[] args)
         {
             #region question 1
@@ -26,18 +30,31 @@
 
 
 
-            int A = 20;
-            Console.WriteLine($"brfore change {A}"); // A= 20
-            ChangeInputValue(A);
-            Console.WriteLine($"after change {A}"); // A= 20
+            //int A = 20;
+            //Console.WriteLine($"brfore change {A}"); // A= 20
+            //ChangeInputValue(A);
+            //Console.WriteLine($"after change {A}"); // A= 20
 
 
 
-            Console.WriteLine($"brfore change {A}"); // A= 20
-            ChangeInputValueByRef(ref A);
-            Console.WriteLine($"after change {A}"); // A= 10
+            //Console.WriteLine($"brfore change {A}"); // A= 20
+            //ChangeInputValueByRef(ref A);
+            //Console.WriteLine($"after change {A}"); // A= 10
 
             #endregion
+
+            #region question 3
+            // 3-	Write a c# Function that accept 4 parameters from user and return result of summation
+            // and subtracting of two numbers
+
+            int A = 10, B = 5, sumNumbers, subNumbers;
+           
+            SumAndSubValues(A,B, out sumNumbers, out subNumbers);
+            Console.WriteLine($"sum: {sumNumbers}, sub:{subNumbers}");
+
+
+            #endregion
+
         }
     }
 }
