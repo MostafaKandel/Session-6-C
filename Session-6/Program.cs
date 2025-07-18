393bc27199924ce48a02848142aff7ec0db8079b
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-07-18 20:40:32 +0200

    Q1

--- Session-6/Program.cs
+++ Session-6/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public static void ChangeInputValue(int x)
+        {
+            x = 10;
+        }
+
+        public static void ChangeInputValueByRef(ref int x) {
+            x = 10;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question 1
+            //1-	Explain the difference between passing (Value type parameters) by value and by reference then
+            //write a suitable c# example.
+            /*
+              
+            - Passing by value: This passes a copy of the value. Any changes made inside the
+            method do NOT affect the original variable.
+             - Passing by reference: This passes reference of the variable.
+                Changes made inside the method DO affect the original variable.
+             */
+
+
+
+            int A = 20;
+            Console.WriteLine($"brfore change {A}"); // A= 20
+            ChangeInputValue(A);
+            Console.WriteLine($"after change {A}"); // A= 20
+
+
+
+            Console.WriteLine($"brfore change {A}"); // A= 20
+            ChangeInputValueByRef(ref A);
+            Console.WriteLine($"after change {A}"); // A= 10
+
+            #endregion
         }
     }
 }
